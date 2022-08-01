cf84f89c8b7052a13bf977a9c0fe1c782e095dda
moonhojun <treehj007@gmail.com>
2022-08-01 12:34:53 +0900

    your_msg

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #-*- coding:utf-8 -*-
 
-from flask import Flask, rander_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return rander_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__" :
     app.run(port=5000)
