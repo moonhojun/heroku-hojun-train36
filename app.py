54a4a08f794958ad89677b99cbf63823da567f2c
moonhojun <treehj007@gmail.com>
2022-08-01 12:07:56 +0900

    your_msg

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #-*- coding:utf-8 -*-
 
-from flask import Flask, rnader_template
+from flask import Flask, rander_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return rnader_template("index.html")
+    return rander_template("index.html")
 
 if __name__ == "__main__" :
     app.run(port=5000)
